bc473141fccf8b0e1cf95d9b650987e257de1736
annashamray <anna.shamray@maykinmedia.nl>
2020-04-06 12:29:18 +0200

    add react support for external services page

--- src/openzaak/config/views.py
+++ src/openzaak/config/views.py
@@ -4,7 +4,14 @@ from django.views.generic import TemplateView, UpdateView
 from extra_views import ModelFormSetView
 from zgw_consumers.models import Service
 
-from .forms import ExternalServiceForm, InternalServiceForm, NLXConfigForm
+from openzaak.components.autorisaties.admin_views import get_form_data
+
+from .forms import (
+    ExternalServiceForm,
+    InternalServiceForm,
+    NLXConfigForm,
+    get_nlx_choices,
+)
 from .models import InternalService, NLXConfig
 from .utils import AdminRequiredMixin
 
@@ -51,6 +58,20 @@ class ExternalConfigView(AdminRequiredMixin, ModelFormSetView):
     model = Service
     queryset = Service.objects.order_by("api_type", "api_root")
     form_class = ExternalServiceForm
-    factory_kwargs = {"extra": 1}
+    factory_kwargs = {"extra": 0}
     template_name = "config/config_external.html"
     success_url = reverse_lazy("config-detail")
+
+    def get_context_data(self, **kwargs):
+        formset = kwargs.pop("formset", self.get_formset())
+        kwargs["formset"] = formset
+
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "formdata": [get_form_data(form) for form in formset],
+                "nlx_choices": get_nlx_choices(),
+            }
+        )
+
+        return context
